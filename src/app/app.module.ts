db84d8d356b638da9c5f92a43e0c376cc56f05d6
Maheshkumar Sellamuthu <sellamuthu.maheshkumar@in.bosch.com>
2019-06-19 11:50:19 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './component/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
-  imports: [
-    BrowserModule,
+  imports: [BrowserModule, FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
